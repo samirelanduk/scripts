e737db592295fbaa56395e1b8bedbdd2c124c8a4
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-26 15:16:11 +0000

    Get deaths per month pages

--- 2016deaths.py
+++ 2016deaths.py
@@ -4,3 +4,11 @@ import calendar
 
 years = [2016]
 months = [calendar.month_name[n + 1] for n in range(12)]
+
+for year in years:
+    for month in months[:3]:
+        # Get page
+        html = requests.get(
+         "https://en.wikipedia.org/wiki/Deaths_in_%s_%i" % (month, year)
+        ).text
+        page = BeautifulSoup(html, "html.parser")
