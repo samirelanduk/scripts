7e594c15bcbfcc1b245a88b549d94a419769ba5e
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-29 16:20:59 +0000

    Change dict keys to date object

--- 2016deaths.py
+++ 2016deaths.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import calendar
+from datetime import date
 import json
 import os
 import numpy
@@ -8,9 +9,9 @@ import numpy
 years = [n + 2000 for n in range(4, 17)]
 months = [calendar.month_name[n + 1] for n in range(12)]
 
-data = []
+data = {}
 for year in years:
-    for month in months[:2]:
+    for month_num, month in enumerate(months, start=1):
         # Get page
         html = requests.get(
          "https://en.wikipedia.org/wiki/Deaths_in_%s_%i" % (month, year)
@@ -21,27 +22,30 @@ for year in years:
         content = page.find("div", {"id": "mw-content-text"})
 
         # Get <li> for each death
-        month_data = {
-         "year": year,
-         "month": month
-        }
-        day = 1
+        day = date(year, month_num, 1)
+        data[day] = []
         list_started = False
         for child in content.findChildren(recursive=False):
             if child.name == "h2":
                 list_started = True
             if child.name == "h3":
-                day = int(child.text.split("[")[0])
-                month_data[day] = []
+                day = date(year, month_num, int(child.text.split("[")[0]))
+                data[day] = []
             elif child.name == "ul" and list_started:
                 for item in child.find_all("li"):
                     first_link = item.a
-                    if first_link:
-                        month_data[day].append([
+                    if first_link and "/wiki/" in first_link["href"]:
+                        data[day].append([
                          first_link.text,
                          first_link["href"]
                         ])
-        data.append(month_data)
 
+# Replace dates with str date
+str_data = dict(data)
+for key in data:
+    str_data[key.strftime("%Y %b %d")] = data[key]
+    del str_data[key]
+
+# Save as .json
 with open("data%sdeaths.json" % os.path.sep, "w") as f:
-    f.write(json.dumps(data))
+    f.write(json.dumps(str_data))
