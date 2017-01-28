cd02c0b934e7bcb12de27df76b1966727b17ff56
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-28 15:59:45 +0000

    Docs builder can push to server

--- builddocs.py
+++ builddocs.py
@@ -15,3 +15,8 @@ project_name = sys.argv[1].split(os.path.sep)[-1]
 os.chdir(project_location)
 os.chdir(os.path.sep.join([project_name, "docs"]))
 subprocess.call("make html", shell=True)
+
+# Push to server
+host = "stage.samireland.com"
+remote_location = "~/docs/"
+subprocess.call("scp -r build/html/* %s:%s%s/" % (host, remote_location, project_name), shell=True)
