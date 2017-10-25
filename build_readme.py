79e2fc38b39d9cc4cfddd27da1fbe2cec475a218
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-25 16:53:11 +0100

    Add badges to README script

--- build_readme.py
+++ build_readme.py
@@ -5,18 +5,36 @@ import os
 import re
 print("")
 
+# Add badges
+blocks = []
+package = os.getcwd().split(os.path.sep)[-1]
+blocks.append(
+ ".. |travis| image:: https://api.travis-ci.org/samirelanduk/{}.svg?branch=master".format(package)
+)
+blocks.append(
+ ".. |coveralls| image:: https://coveralls.io/repos/github/samirelanduk/{}/badge.svg?branch=master".format(package)
+)
+blocks.append(
+".. |pypi| image:: https://img.shields.io/pypi/pyversions/{}.svg".format(package)
+)
+blocks.append(
+"\n|travis| |coveralls| |pypi|"
+)
+
+
 # Get text blocks
 files = ["installing.rst", "overview.rst", "changelog.rst"]
 with open("docs/source/index.rst") as f:
     block = f.read()
 block = block.split("Table of Contents")[0]
-blocks = [block]
+blocks.append(block)
 for file_name in files:
     with open("docs/source/%s" % file_name) as f:
         blocks.append(f.read())
 
 # Remove pointless RST formatting
 blocks = [re.sub(r":py:(.+?):\`~*\.(.+?)\`", r"``\2``", block) for block in blocks]
+
 
 # Save as README.rst
 with open("README.rst", "w") as f:
