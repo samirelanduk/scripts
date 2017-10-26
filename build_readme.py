252ab698075eac8e45ffb34c3bce79f7fe925ab1
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-26 07:08:01 +0100

    Make README script use branches

--- build_readme.py
+++ build_readme.py
@@ -3,16 +3,21 @@
 import sys
 import os
 import re
+import subprocess
 print("")
 
 # Add badges
 blocks = []
 package = os.getcwd().split(os.path.sep)[-1]
+branch = subprocess.check_output("git branch", shell=True).decode()
+branches = branch.split("\n")
+current_branch = [branch for branch in branches if branch.startswith("*")][0]
+branch = current_branch.split()[1]
 blocks.append(
- ".. |travis| image:: https://api.travis-ci.org/samirelanduk/{}.svg?branch=master".format(package)
+ ".. |travis| image:: https://api.travis-ci.org/samirelanduk/{}.svg?branch={}".format(package, branch)
 )
 blocks.append(
- ".. |coveralls| image:: https://coveralls.io/repos/github/samirelanduk/{}/badge.svg?branch=master".format(package)
+ ".. |coveralls| image:: https://coveralls.io/repos/github/samirelanduk/{}/badge.svg?branch={}".format(package, branch)
 )
 blocks.append(
 ".. |pypi| image:: https://img.shields.io/pypi/pyversions/{}.svg".format(package)
