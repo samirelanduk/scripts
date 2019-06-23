b6172f5b5ff4f0d35e63023932ff3e6d3060cbb0
Sam Ireland <sam.ireland.uk@gmail.com>
2019-06-23 10:46:10 +0100

    Improve README script

--- build_readme.py
+++ build_readme.py
@@ -4,18 +4,25 @@ import sys
 import os
 import re
 import subprocess
+import requests
+import json
 print("")
+
+# Use downloads?
+package = os.getcwd().split(os.path.sep)[-1]
+downloads = requests.get("https://pypistats.org/api/packages/{}/recent".format(package)).json()["data"]["last_month"] > 100
 
 # Add badges
 blocks = []
-package = os.getcwd().split(os.path.sep)[-1]
 branch = subprocess.check_output("git branch", shell=True).decode()
 branches = branch.split("\n")
 current_branch = [branch for branch in branches if branch.startswith("*")][0]
 branch = current_branch.split()[1]
-
 blocks.append(
-"|travis| |coveralls| |pypi|"
+ package + "\n" + "=" * len(package)
+)
+blocks.append(
+"|travis| |coveralls| |pypi| |version| |commit|" + (" |downloads|" if downloads else "")
 )
 
 blocks.append(
@@ -36,6 +43,27 @@ blocks.append(
 blocks[-1] += (
  "\n  :target: https://pypi.org/project/{}/".format(package)
 )
+blocks.append(
+".. |version| image:: https://img.shields.io/pypi/v/{}.svg".format(package)
+)
+blocks[-1] += (
+ "\n  :target: https://pypi.org/project/{}/".format(package)
+)
+blocks.append(
+".. |commit| image:: https://img.shields.io/github/last-commit/samirelanduk/{}/{}.svg".format(package, branch)
+)
+blocks[-1] += (
+ "\n  :target: https://github.com/samirelanduk/{}/tree/{}/".format(package, branch)
+)
+
+if downloads:
+    blocks.append(
+    ".. |downloads| image:: https://img.shields.io/pypi/dm/{}.svg".format(package)
+    )
+    blocks[-1] += (
+     "\n  :target: https://pypi.org/project/{}/".format(package)
+    )
+    downloads = True
 
 
 
@@ -52,7 +80,7 @@ for file_name in files:
 # Remove pointless RST formatting
 blocks = [re.sub(r":py:(.+?):\`~*\.(.+?)\`", r"``\2``", block) for block in blocks]
 
-
+blocks[7 + downloads] = "\n".join(blocks[7 + downloads].split("\n")[2:])
 # Save as README.rst
 with open("README.rst", "w") as f:
     f.write("\n\n".join(blocks))
